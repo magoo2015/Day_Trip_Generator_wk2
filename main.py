9088227496bc9b3e1db141c10251bf3557e4eb71
Sydney McGee <mcgee.sydney@gmail.com>
2022-05-05 11:13:33 -0500

    Update main.py with current progress

--- main.py
+++ main.py
@@ -7,14 +7,41 @@ restaurant_list = ["Texas RoadHouse", "Rafains", "Hard Rock Cafe", "Toby Keith B
 transportation_list = ["Bus", "Train", "Horse Carriage", "Uber/LIFT", "Rapid Transit"]
 entertainment_list = ["Six Flags", "NBA Game", "UFC: Israel Adesayna vs. Robert Whitaker", "Sea World", "WWE WrestleMania"]
 
-#Stuck on selecting the destinatin randomly
+user_trip_details = []
+#Stuck on selecting the destinati0n randomly
 def user_destination():
     random_destinations = random.choice(destinations_list)
+    return random_destinations
     
     
-        
+user_selected_destination = user_destination()
 
-user_destination()
+
+
+
+
+def trip_generator():
+    trip_on = True
+    print("Welcome to the Day Trip Generator, where we help you plan your vacation!")
+    choice = input(f"We have selected {user_selected_destination} as your destination.  How does this sound?")
+    while trip_on:
+        if choice == "y":
+            print("Awesome great choice. Let's continue")
+            user_trip_details += user_selected_destination
+            print(user_trip_details)
+            trip_on = False
+        else:
+            trip_on = True
+
+trip_generator()
+
+
+    
+
+
+
+
+
 
 
 
