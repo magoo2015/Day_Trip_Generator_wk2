0310e2280a17b88d6219f4aea9a7b70f9c75bc2c
Sydney McGee <mcgee.sydney@gmail.com>
2022-05-04 23:33:32 -0500

    Update main.py with current progress on project

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+import random # For random selections
+
 #Lists for destinations, restaurants, transportation, and entertainment.
 
-destinations_list = ["Dallas, Tx", "Atlanta, Ga", "Baton Rouge, LA", "Denver, Colorado"]
+destinations_list = ["Dallas", "Atlanta", "Baton Rouge", "Denver"]
 restaurant_list = ["Texas RoadHouse", "Rafains", "Hard Rock Cafe", "Toby Keith Bar and Grill", "Glorias" ]
 transportation_list = ["Bus", "Train", "Horse Carriage", "Uber/LIFT", "Rapid Transit"]
 entertainment_list = ["Six Flags", "NBA Game", "UFC: Israel Adesayna vs. Robert Whitaker", "Sea World", "WWE WrestleMania"]
+
+#Stuck on selecting the destinatin randomly
+def user_destination():
+    random_destinations = random.choice(destinations_list)
+    
+    
+        
+
+user_destination()
+
+
+
+
+
+
+
+
+
+
