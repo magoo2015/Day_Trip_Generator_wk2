b7df679e112c4d0b54e5a75a2f201149f226d89f
Sydney McGee <mcgee.sydney@gmail.com>
2022-05-04 22:12:33 -0500

    Add project lists to main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+#Lists for destinations, restaurants, transportation, and entertainment.
+
+destinations_list = ["Dallas, Tx", "Atlanta, Ga", "Baton Rouge, LA", "Denver, Colorado"]
+restaurant_list = ["Texas RoadHouse", "Rafains", "Hard Rock Cafe", "Toby Keith Bar and Grill", "Glorias" ]
+transportation_list = ["Bus", "Train", "Horse Carriage", "Uber/LIFT", "Rapid Transit"]
+entertainment_list = ["Six Flags", "NBA Game", "UFC: Israel Adesayna vs. Robert Whitaker", "Sea World", "WWE WrestleMania"]
